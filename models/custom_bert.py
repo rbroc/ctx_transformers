e2d0bb426d36dceb80097a73aae70d8d42938b06
rbroc <rbrrcc@gmail.com>
2020-12-08 14:19:18 +0100

    encode through loop

--- models/custom_bert.py
+++ models/custom_bert.py
@@ -10,8 +10,26 @@ class CustomBertLayer(layers.Layer):
         self.trainable = trainable
         self.output_signature = tf.float32
 
-    def call(self, input):
-        enc = tf.map_fn(lambda x: self.model({'input_ids': x[0], 
+    @tf.function
+    def _enc_loop(self, x):
+        c = 0
+        enc = tf.TensorArray(tf.float32, size=0, 
+                            dynamic_size=True)
+        inptup = tf.stack([x[0], x[1]], axis=1)
+        for i in inptup:
+            ienc = self.model({'input_ids':
+                                tf.reshape(i[0], [1, -1]), 
+                                'attention_mask': 
+                                tf.reshape(i[1], [1, -1])})[0][:,0,:]
+            enc = enc.write(c, ienc[0])
+            c += 1
+        return enc.stack()
+
+    def call(self, input, low_mem=True):
+        if low_mem:
+            enc = tf.map_fn(self._enc_loop, elems=tuple(input), dtype=self.output_signature)
+        else:
+            enc = tf.map_fn(lambda x: self.model({'input_ids': x[0], 
                                               'attention_mask': x[1]})[0][:,0,:],
                         elems=tuple(input), 
                         dtype=self.output_signature)
