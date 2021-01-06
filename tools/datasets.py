bd8d45d48003e55a3ad6b25e500045b490ba6c02
rbroc <rbrrcc@gmail.com>
2021-01-06 16:12:19 +0100

    add tpu and add graph functionality

--- tools/datasets.py
+++ tools/datasets.py
@@ -11,11 +11,92 @@ from tensorflow.io import FixedLenFeature
 from IPython.display import clear_output
 import abc
 
+#### Feature spec
+
+FEATURE_DESCRIPTION_SIMPLE = {
+    'input_ids': FixedLenFeature([], tf.string),
+    'attention_mask': FixedLenFeature([], tf.string),
+    'label': FixedLenFeature([], tf.int64),
+}
+
 
 FEATURE_DESCRIPTION_TRIPLET = {
     'input_ids': FixedLenFeature([], tf.string),
     'attention_mask': FixedLenFeature([], tf.string)
 }
+
+
+######### SIMPLE NETWORK #########
+
+def _make_example_simple_nn1(input_ids, attention_mask, label):
+    ''' Example serializer'''
+    input_ids_feature = Feature(
+        bytes_list=BytesList(value=[
+            tf.io.serialize_tensor(input_ids).numpy(),
+        ])
+    )
+    attention_mask_feature = Feature(
+        bytes_list=BytesList(value=[
+            tf.io.serialize_tensor(attention_mask).numpy(),
+        ])
+    )
+
+    label_feature = Feature(int64_list=tf.train.Int64List(value=[label]))
+
+    features = Features(feature={
+        'input_ids': input_ids_feature,
+        'attention_mask': attention_mask_feature,
+        'label': label_feature,
+    }) 
+
+    example = Example(features=features)
+    return example.SerializeToString()
+
+
+def _map_fn_simple_nn1(x, y):
+    ''' Maps read example function to whole dataset'''
+    tf_string = tf.py_function(func=_make_example_simple_nn1, 
+                               inp=[x[0], x[1], y], 
+                               Tout=tf.string)
+    return tf_string
+
+
+def save_tfrecord_simple_nn1(dataset, filename=None, 
+                      shard=False, n_shards=20, 
+                      path='datasets/test/',
+                      compression=None):
+    ''' Saves tfrecord using functions defined above'''
+    dataset = dataset.map(_map_fn_simple_nn1)
+    if shard:
+        dataset = dataset.enumerate()
+        dataset = dataset.apply(tf.data.experimental.group_by_window(
+        lambda i, _: i % n_shards, 
+        lambda k, d: _reduce_fn(k, d, compression, n_shards, path), 
+        tf.int64.max
+        ))
+        for s in dataset:
+            clear_output(wait=True)
+            print(f'Saving {s} ...')
+    else:
+        writer = tf.data.experimental.TFRecordWriter(path + filename)
+        writer.write(dataset.map(lambda _, x: x))
+
+
+def _parse_fn_simple_nn1(example):
+    example = tf.io.parse_single_example(example, FEATURE_DESCRIPTION_SIMPLE)
+    input_ids = tf.io.parse_tensor(example['input_ids'], tf.int32)
+    attention_mask = tf.io.parse_tensor(example['attention_mask'], tf.int32)
+    label = tf.cast(example['label'], tf.int32)
+    return dict(zip(['input_ids', 'attention_mask'], [input_ids, attention_mask])), \
+           dict(zip(['label'], [label]))
+
+
+def load_tfrecord_simple_nn1(filenames, **kwargs):
+    dataset = tf.data.TFRecordDataset(filenames, **kwargs)
+    return dataset.map(_parse_fn_simple_nn1)
+
+
+######### TRIPLET LOSS NETWORK #########
 
 def _make_example_triplet_nn1(input_ids, attention_mask):
     ''' Example serializer'''
@@ -65,6 +146,7 @@ def save_tfrecord_triplet_nn1(dataset, filename=None,
         writer = tf.data.experimental.TFRecordWriter(path + filename)
         writer.write(dataset.map(lambda _, x: x))
 
+
 def _parse_fn_triplet_nn1(example):
     example = tf.io.parse_single_example(example, FEATURE_DESCRIPTION_TRIPLET)
     input_ids = tf.io.parse_tensor(example['input_ids'], tf.int32)
@@ -72,9 +154,21 @@ def _parse_fn_triplet_nn1(example):
     return dict(zip(['input_ids', 'attention_mask'], [input_ids, attention_mask]))
 
 
-def load_tfrecord_triplet_nn1(filenames, **kwargs):
-    dataset = tf.data.TFRecordDataset(filenames, **kwargs)
-    return dataset.map(_parse_fn_triplet_nn1)
+def load_tfrecord_triplet_nn1(filenames, 
+                              num_parallel_calls=1,
+                              deterministic=False,
+                              cycle_length=16,
+                              compression_type='GZIP'):
+    opts = tf.data.Options()
+    opts.experimental_deterministic = deterministic
+    dataset = tf.data.Dataset.from_tensor_slices(filenames)
+    dataset = dataset.with_options(opts)
+    read_fn = lambda x: tf.data.TFRecordDataset(x, compression_type=compression_type)
+    dataset = dataset.interleave(read_fn, 
+                                 cycle_length=cycle_length, 
+                                 num_parallel_calls=num_parallel_calls)
+    return dataset.map(_parse_fn_triplet_nn1, 
+                       num_parallel_calls=num_parallel_calls)
 
 
 
@@ -87,6 +181,7 @@ def _reduce_fn(key, dataset, compression, n_shards, path):
                                                  compression_type=compression)
     writer.write(dataset.map(lambda _, x: x))
     return tf.data.Dataset.from_tensors(filename)
+
 
 
 ######################## DRAFT STRUCTURE ########################
