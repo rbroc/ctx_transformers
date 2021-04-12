f0a7bba3cb42fa6913d132426485b21fb5945911
rbroc <rbrrcc@gmail.com>
2021-04-12 11:30:08 +0200

    add tfdata padding utils + triplet user index

--- reddit/utils/__init__.py
+++ reddit/utils/__init__.py
@@ -1,6 +1,7 @@
 from .tfrecords import (save_tfrecord, 
                         load_tfrecord)
-from .datasets import split_dataset
+from .datasets import (split_dataset,
+                       pad_and_stack)
 from .compute import (compute_mean_pairwise_distance,
                       average_encodings)
 
@@ -8,5 +9,6 @@ from .compute import (compute_mean_pairwise_distance,
 __all__ = ['save_tfrecord',
            'load_tfrecord',
            'split_dataset', 
+           'pad_and_stack',
            'average_encodings', 
            'compute_mean_pairwise_distance']
--- reddit/utils/datasets.py
+++ reddit/utils/datasets.py
@@ -1,6 +1,52 @@
 import tensorflow as tf
 
 
+def pad_and_stack(dataset, pad_to=[20,1,1]):
+    ''' Pads the dataset according to specified number of posts 
+        passed via pad_to (anchor, positive, negative) and stacks
+        negative, positive and anchor posts vertically
+    Args:
+        dataset (TFDataset): dataset to pad and stack 
+        pad_to (list or tuple): list containing number of posts
+            to pad to, i.e., [n_anchor_posts, n_positive_posts,
+            n_negative_posts]
+    '''
+    dataset = dataset.map(lambda x: {'iids': x['iids'][:pad_to[0],:], 
+                                     'amask': x['amask'][:pad_to[0],:],
+                                     'pos_iids': x['pos_iids'][:pad_to[1],:],
+                                     'pos_amask': x['pos_amask'][:pad_to[1],:],
+                                     'neg_iids': x['neg_iids'][:pad_to[2],:],
+                                     'neg_amask': x['neg_amask'][:pad_to[2],:],
+                                     'author_id': x['author_id']})
+    dataset = dataset.map(lambda x: {'iids': tf.pad(x['iids'], 
+                                                    [[0,pad_to[0]-tf.shape(x['iids'])[0]],
+                                                     [0,0]]),
+                                     'amask': tf.pad(x['amask'], 
+                                                     [[0,pad_to[0]-tf.shape(x['amask'])[0]],
+                                                      [0,0]]), 
+                                     'pos_iids': tf.pad(x['pos_iids'], 
+                                                        [[0,pad_to[1]-tf.shape(x['pos_iids'])[0]],
+                                                         [0,0]]),
+                                     'pos_amask': tf.pad(x['pos_amask'], 
+                                                         [[0,pad_to[1]-tf.shape(x['pos_amask'])[0]],
+                                                          [0,0]]), 
+                                     'neg_iids': tf.pad(x['neg_iids'], 
+                                                        [[0,pad_to[2]-tf.shape(x['neg_iids'])[0]],
+                                                         [0,0]]),
+                                     'neg_amask': tf.pad(x['neg_amask'], 
+                                                         [[0,pad_to[2]-tf.shape(x['neg_amask'])[0]],
+                                                          [0,0]]),
+                                     'author_id': x['author_id']})
+    dataset = dataset.map(lambda x: {'input_ids': tf.concat([x['neg_iids'],
+                                                             x['pos_iids'],
+                                                             x['iids']], axis=0),
+                                     'attention_mask': tf.concat([x['neg_amask'],
+                                                                  x['pos_amask'],
+                                                                  x['amask']], axis=0),
+                                     'id': x['author_id']})
+    return dataset
+    
+
 def split_dataset(dataset, size=None,
                   perc_train=.7, perc_val=.1, 
                   tuning=None):
